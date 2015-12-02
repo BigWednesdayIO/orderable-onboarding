c9ced154a9aaf32561ebb2ffd771db0abdfce2e7
Michael Strutt <michael@mstrutt.co.uk>
2015-12-02 13:41:24 +0000

    refining dashboard and empty states

--- app/assets/js/controllers/dashboard.js
+++ app/assets/js/controllers/dashboard.js
@@ -8,55 +8,9 @@ function DashboardController ($stateParams, setupSteps) {
 }
 
 DashboardController.resolve = /* @ngInject */ {
-	supplierInfo: function(supplierService) {
-		return supplierService
-			.getInfo();
-	},
-	products: function(productService) {
-		return productService
-			.getProducts();
-	},
-	setupSteps: function($sce, products, supplierInfo) {
-		var pending = [];
-		var completed = [];
-
-		function safeDescription (step) {
-			step.description = $sce.trustAsHtml(step.description);
-			return step;
-		}
-
-		if (products.length === 0) {
-			pending.push({
-				name: 'Add some products',
-				description: 'To sell to retailers you\'ll need to add your supplies to Orderable',
-				action: 'products/add/?first=true',
-				icon: 'tag-multiple'
-			})
-		} else {
-			completed.push({
-				name: 'You\'ve added ' + products.length + ' product' + (products.length === 1 ? '' : 's'),
-				description: 'But you can always <a href="products/add/">add more</a>'
-			});
-		}
-
-		pending = pending.concat([
-			{
-				name: 'Set up a payment method',
-				description: 'Orderable uses <a href="https://stripe.com/gb" target="_blank">Stripe</a> to securely handle payments for you. To get paid you\'ll need to set up an account',
-				action: 'account/',
-				icon: 'payment'
-			}, {
-				name: 'Tell us more about ' + supplierInfo.name,
-				description: 'Paper invoices are a thing of the past! Orderable takes away the hastle and automatically generates invoices for retailers on purchase. To do that we need a few things from you',
-				action: 'account/',
-				icon: 'info'
-			}
-		]);
-
-		return {
-			pending: pending.map(safeDescription),
-			completed: completed.map(safeDescription)
-		};
+	setupSteps: function(setupStepsService) {
+		return setupStepsService
+			.getSetupSteps();
 	}
 };
 
---
+++ app/assets/js/services/setup-setps.js
@@ -0,0 +1,71 @@
+function SetupStepsService ($sce, $q, productService, supplierService) {
+	var service = this;
+
+	function safeDescription (step) {
+		step.description = $sce.trustAsHtml(step.description);
+		return step;
+	}
+
+	function atLeastOneEnabled (paymentMethods) {
+		return paymentMethods.some(function(method) {
+			return method.enabled;
+		})
+	}
+
+	service.getSetupSteps = function() {
+		return $q.all([
+			supplierService
+				.getInfo(),
+			supplierService
+				.getPaymentMethods(),
+			productService
+				.getProducts()
+		])
+			.then(function(responses) {
+				var supplierInfo = responses[0];
+				var paymentMethods = responses[1];
+				var products = responses[2];
+
+				var pending = [];
+				var completed = [];
+
+				if (products.length === 0) {
+					pending.push({
+						name: 'Add some products',
+						description: 'To sell to retailers you\'ll need to add your supplies to Orderable',
+						action: 'products/add/?first=true',
+						icon: 'tag-multiple'
+					})
+				} else {
+					completed.push({
+						name: 'You\'ve added ' + products.length + ' product' + (products.length === 1 ? '' : 's'),
+						description: 'But you can always <a href="products/add/">add more</a>'
+					});
+				}
+
+				if (atLeastOneEnabled(paymentMethods)) {
+					completed.push({
+						name: 'You\'re all ready to take payments',
+						description: 'Payment methods can added or removed from <a href="account/">your account</a> at any time'
+					});
+				} else {
+					pending.push({
+						name: 'Set up a payment method',
+						description: 'Orderable uses <a href="https://stripe.com/gb" target="_blank">Stripe</a> to securely handle payments for you. To get paid you\'ll need to set up an account',
+						action: 'account/',
+						icon: 'payment'
+					});
+				}
+
+				return {
+					pending: pending.map(safeDescription),
+					completed: completed.map(safeDescription)
+				};
+			});
+	};
+}
+
+angular
+	.module('app')
+	.service('setupStepsService', SetupStepsService);
+	
