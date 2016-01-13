4bcb029f50c027d1f62c4a9b9dfdc36d681058c2
Michael Strutt <michael@mstrutt.co.uk>
2016-01-13 13:16:18 +0000

    upping default page size for products returned

--- app/assets/js/services/product.js
+++ app/assets/js/services/product.js
@@ -29,7 +29,7 @@ function ProductService ($http, $q, API, authenticationService, _) {
 
 		return $http({
 			method: 'GET',
-			url: API.suppliers + '/' + id + '/linked_products?expand[]=product'
+			url: API.suppliers + '/' + id + '/linked_products?expand[]=product&hitsPerPage=100'
 		});
 	};
 
