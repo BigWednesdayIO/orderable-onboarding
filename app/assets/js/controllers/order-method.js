89b3f47c72b5d5f50c33428b1758d3da4116473d
Michael Strutt <michael@mstrutt.co.uk>
2016-03-04 12:31:56 +0000

    basic UI for setting alternate email for orders to be sent to, option for choosing SMS notifications (incomplete)

---
+++ app/assets/js/controllers/order-method.js
@@ -0,0 +1,32 @@
+function OrderMethodController ($state, supplierService, supplierInfo) {
+	var vm = this;
+
+	vm.supplier = supplierInfo;
+
+	vm.customOrderEmail = !!supplierInfo.orders_email;
+
+	vm.textNotifications = !!supplierInfo.orders_textsms;
+
+	vm.updateOrderMethod = function() {
+		var info = {
+			orders_email: (vm.customOrderEmail ? vm.supplier.orders_email : '')
+		};
+
+		supplierService
+			.updateInfo(info)
+			.then(function() {
+				$state.go('account');
+			});
+	};
+}
+
+OrderMethodController.resolve = /* @ngInject */ {
+	supplierInfo: function(supplierService) {
+		return supplierService
+			.getInfo();
+	}
+};
+
+angular
+	.module('app')
+	.controller('OrderMethodController', OrderMethodController);
